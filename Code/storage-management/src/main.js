b8458e87dd1dc9fe2bb198d114a8a06f8be628b8
Bill ZHONG <billzhonggz@outlook.com>
2017-06-06 11:45:58 +0800

    Configured Vue Router.

--- Code/storage-management/src/main.js
+++ Code/storage-management/src/main.js
@@ -2,14 +2,21 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import router from './router'
+import VueRouter from 'vue-router'
+import routerConfig from './router-config'
+
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  routes: routerConfig
+})
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
   router,
-  template: '<App/>',
-  components: { App }
+  // store,
+  el: '#app',
+  render: h => h(App)
 })
---
+++ Code/storage-management/src/router-config.js
@@ -0,0 +1,11 @@
+import sell from './pages/sell/index.vue'
+import storage from './pages/storage/index.vue'
+
+export default [
+  {
+    path: '/sell', component: sell
+  },
+  {
+    path: '/storage', component: storage
+  }
+]
