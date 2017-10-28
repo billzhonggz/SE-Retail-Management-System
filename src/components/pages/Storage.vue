63dc9f4d3c4c6e560b13be93f09396b02ff321e4
Bill ZHONG <billzhonggz@outlook.com>
2017-10-28 14:06:27 +0800

    Establish the basic UI for storage management page.

--- src/components/pages/Storage.vue
+++ src/components/pages/Storage.vue
@@ -1,8 +1,115 @@
 <template>
   <div id="app">
-    <h1>You are in storage management page.</h1>
+    <!--Body Part-->
+      <!--<el-row>-->
+    <main>
+      <el-col v-bind:span="12" :offset="6">
+        <el-row>
+          <el-tabs type="border-card" v-model="cateIndex">
+            <el-tab-pane v-for="category in categories" :key="category.id" :label="category.title">
+              <el-card class="box-card">
+                <el-row :span="4" :offset="20">
+                  <el-button type="success">ADD NEW ITEM</el-button>
+                </el-row>
+              </el-card>
+              <el-card class="box-card" v-for="item in category.commodity" :key="item.id">
+                <div slot="header" class="clearfix">
+                  <!--Card name-->
+                  <el-row>
+                    <el-col v-bind:span="4">
+                      <strong style="line-height: 36px;">Name</strong>
+                    </el-col>
+                    <el-col v-bind:span="4">
+                      <strong style="line-height: 36px;">{{item.name}}</strong>
+                    </el-col>
+                    <el-col v-bind:span="4" :offset="8">
+                      <el-button type="primary">EDIT</el-button>
+                    </el-col>
+                    <el-col v-bind:span="4">
+                      <el-button type="danger">DELETE</el-button>
+                    </el-col>
+                  </el-row>
+                </div>
+                <el-row>
+                  <el-col :span="12">
+                    <div class="text item">
+                      <!--List of content-->
+                      <p>Unit: {{item.unit}}</p>
+                      <p>Unit price: {{item.price}}</p>
+                      <p>Amount available: {{item.amount}}</p>
+                      <p>Status: {{item.state}}</p>
+                    </div>
+                  </el-col>
+                  <el-col :span="12">
+                    <img style="max-width: 100%; height: auto; float: right;" src="../../pic/apple.jpg" class="image">
+                  </el-col>
+                </el-row>
+              </el-card>
+            </el-tab-pane>
+          </el-tabs>
+        </el-row>
+      </el-col>
+    </main>
   </div>
 </template>
+
+<script>
+  /* eslint-disable quotes,semi */
+
+  import ElRow from "element-ui/packages/row/src/row";
+  import ElCol from "element-ui/packages/col/src/col";
+  import ElTabPane from "../../../node_modules/element-ui/packages/tabs/src/tab-pane";
+  //  import ElInputNumber from '../node_modules/element-ui/packages/input-number'
+  import ElInput from "../../../node_modules/element-ui/packages/input/index";
+  import ElButton from "../../../node_modules/element-ui/packages/button/src/button";
+  import ElCard from "../../../node_modules/element-ui/packages/card/src/main";
+  import firebase from "firebase";
+
+  //  let config = {
+  //    apiKey: 'AIzaSyDxF-ZmWZP0qtgeQQli2oPCTn6hw4aovbo',
+  //    authDomain: 'se-retail.firebaseapp.com',
+  //    databaseURL: 'https://se-retail.firebaseio.com',
+  //    projectId: 'se-retail',
+  //    storageBucket: 'se-retail.appspot.com',
+  //    messagingSenderId: '193615524398'
+  //  }
+
+  let config = {
+    apiKey: "AIzaSyBhr-1yhcgKdqiyYwPRWJ2DatlzxzonHhs",
+    authDomain: "se-retail-management.firebaseapp.com",
+    databaseURL: "https://se-retail-management.firebaseio.com",
+    projectId: "se-retail-management",
+    storageBucket: "se-retail-management.appspot.com",
+    messagingSenderId: "729149844491"
+  };
+
+  let app = firebase.initializeApp(config);
+  let db = app.database();
+  let dbRef = {
+    source: db.ref("categories")
+    // asObject: true
+  };
+  let dbOrd = {
+    source: db.ref("orders"),
+    asObject: true
+  };
+
+  export default {
+    firebase: {
+      categories: dbRef,
+      //      commodities: dbCom,
+      orders: dbOrd
+    },
+    components: {
+      ElInput,
+      ElCard,
+      ElButton,
+      ElTabPane,
+      ElCol,
+      ElRow
+    }
+  }
+</script>
 
 <style>
   #app {
